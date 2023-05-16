430cbb9293ef4629cee1ecbd0b54dee0d993aa05
Kadu <moreira.carloseduardo@gmail.com>
2023-05-16 17:26:30 -0300

    Spikes

--- Assets/Scripts/Player_.cs
+++ Assets/Scripts/Player_.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Player_ : MonoBehaviour {
     void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == "FireParticle") {
+        if (other.gameObject.tag == "FireParticle" || other.gameObject.tag == "Spike") {
             Game_.instance.rule_.gameStates.ChangeState(GameStates.Lose);
-            Debug.Log("Fire");
+            Debug.Log("Morreu");
         }
     }
 }
