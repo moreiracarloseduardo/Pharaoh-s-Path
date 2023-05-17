9d40c8b8778cf29e08d63b5488d933aed1d87b9b
Kadu <moreira.carloseduardo@gmail.com>
2023-05-17 18:45:41 -0300

    Ink Mechanic Fixes #11

--- Assets/Scripts/InkItem_.cs
+++ Assets/Scripts/InkItem_.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InkItem_ : MonoBehaviour {
-    public float refillAmount = 1;  
+    public float refillAmount = 1;
+    public float rotationSpeed = 50f; 
+    public float floatSpeed = 0.5f; 
+    public float floatRange = 0.5f; 
+
+    private Vector3 initialPosition;
+
+    void Start() {
+        initialPosition = transform.position;
+        LeanTween.moveY(gameObject, initialPosition.y + floatRange, floatSpeed).setEase(LeanTweenType.pingPong);
+    }
+
+    void Update() {
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player")) {
             Game_.instance.inkAmount += refillAmount;
-            Destroy(gameObject);  
+            Destroy(gameObject);
         }
     }
 }
